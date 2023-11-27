335a3e1bc85d8536f56ad529eae3edd55b35d0be
Rick Lodewijk <Rick.Lodewijk@talnet.nlgit config user.name git config user.emailgit config --global user.email Rick.Lodewijk@talnet.nl>
2023-11-27 13:44:30 +0100

    europa doet het

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=world',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
+
+?>
---
+++ views/country/index.php
@@ -0,0 +1,57 @@
+<?php
+
+use app\models\Country;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/** @var yii\web\View $this */
+/** @var app\models\CountrySearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Countries';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="country-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Country', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'Code',
+            'Name',
+            'Continent',
+            'Region',
+            'SurfaceArea',
+            //'IndepYear',
+            //'Population',
+            //'LifeExpectancy',
+            //'GNP',
+            //'GNPOld',
+            //'LocalName',
+            //'GovernmentForm',
+            //'HeadOfState',
+            //'Capital',
+            //'Code2',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Country $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'Code' => $model->Code]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
