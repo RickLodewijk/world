74c6da3beeb4d2ba7cd0314c2e16c82148bb750a
Rick Lodewijk <Rick.Lodewijk@talnet.nlgit config user.name git config user.emailgit config --global user.email Rick.Lodewijk@talnet.nl>
2023-12-04 09:12:43 +0100

    debugg ready

---
+++ config/functions.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Debug function
+ * _d($var);
+ */
+function _d($var,$caller=null)
+{
+    if(!isset($caller)){
+        $caller = debug_backtrace(1)[0];
+    }
+    echo '<code>Line: '.$caller['line'].'<br>';
+    echo 'File: '.$caller['file'].'</code>';
+    echo '<pre>';
+    yii\helpers\VarDumper::dump($var, 10, true);
+    echo '</pre>';
+}
+
+/**
+ * Debug function with die() after
+ * _dd($var);
+ */
+function _dd($var)
+{
+    $caller = debug_backtrace(1)[0];
+    d($var,$caller);
+    die();
+}
+?>
